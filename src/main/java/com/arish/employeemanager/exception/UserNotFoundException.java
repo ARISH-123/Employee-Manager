635ad7c0ab5fc6e434b7942493b1f7fe4c2f78e6
Arish Ahmad <arish.ahmad1999@gmail.com>
2025-07-24 17:34:43 +0530

    first commit

---
+++ src/main/java/com/arish/employeemanager/exception/UserNotFoundException.java
@@ -0,0 +1,7 @@
+package com.arish.employeemanager.exception;
+
+public class UserNotFoundException extends RuntimeException {
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+}
